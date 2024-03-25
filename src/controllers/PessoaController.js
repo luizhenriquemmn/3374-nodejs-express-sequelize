c60ff9a6a8a05eaff92ab4c72644621ab44bb87d
Luiz Henrique <luizhenrique.mmn@gmail.com>
2024-03-24 20:13:52 -0400

    primeiro comit

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+  static async pegaTodas(req, res) {
+    try {
+      //Acessar o modelo
+    } catch (erro) {
+      //tratamento de erro
+    }
+  }
+}
+
+module.exports = PessoaController;
